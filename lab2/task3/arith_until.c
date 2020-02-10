a0ee8d3fd821e4d4b4df6a362d66af326ab71442
Spencer Poisseroux <spoisseroux@me.com>
2020-02-09 20:50:05 -0500

    lab 2 : task 3 [update]

--- lab2/task3/arith_until.c
+++ lab2/task3/arith_until.c
@@ -6,22 +6,25 @@
 
 int main()
 {
-    float r, a, x, b;
-    printf("Enter radius (in millimeters):\n");
-    scanf("%f", &r);
     
-    if (r == 0) {
-        printf("Exit");
-        exit(0);
-    } else {
-
+    float r, a, x, b;
+    r = 1;
+    
+    while(r != 0) {
+        printf("Enter radius (in millimeters):\n");
+        scanf("%f", &r);
+        
+        if (r == 0) {
+            printf("Exit");
+            exit(0);
+        }
+        
         x = r / 25.4;
         a = PI * x * x;
         b = 2 * PI * x;
 
         printf("Circle's area is %3.2f (sq in).\n", a);
-        printf("Circle's circumfrence is is %3.2f (in).\n", b);
-        main();
+        printf("Circle's circumfrence is %3.2f (in).\n", b);
     }
 }
 
