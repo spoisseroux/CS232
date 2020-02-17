e1cb21a798b954c8366976337a218b2b80bad0b3
Spencer Poisseroux <spoisseroux@me.com>
2020-02-17 16:25:56 -0500

    lab 3 : task 2

--- lab3/task2/analyse.c
+++ lab3/task2/analyse.c
@@ -7,46 +7,40 @@
 
 int main()
 {
-  char text[MAX], c;
-  int i;
-  int lowercase, uppercase, digits, other;
-  int length;
+    char text[MAX], c;
+    int i;
+    int lowercase, uppercase, digits, other;
+    int length;
   
-  puts("Type some text (then ENTER):");
-  
-  /* Save typed characters in text[]: */
+    puts("Type some text (then ENTER):");
     
-  for (i = 0; i < MAX; i++)
-  {
-    text[i] = getchar();
-    if (text[i] == '\n')
-      break;
-  }
-  length = i;
+    fgets(text, MAX, stdin);
+    
+    length = strlen(text) - 1;
   
   /* Analyse contents of text[]: */
     
-  for (i = lowercase = uppercase = digits = other = 0; i < MAX; i++)
-  {
-    c = text[i];
-    if (c >= 'a' && c <= 'z')
-      lowercase++;
-    else if (c >= 'A' && c <= 'Z')
-      uppercase++;
-    else if (c >= '0' && c <= '9')
-      digits++;
-    else
+    for (i = lowercase = uppercase = digits = other = 0; i < MAX; i++)
     {
-      if (c == '\n')
-        break;
-      other++;
+        c = text[i];
+        if (c >= 'a' && c <= 'z')
+            lowercase++;
+        else if (c >= 'A' && c <= 'Z')
+            uppercase++;
+        else if (c >= '0' && c <= '9')
+            digits++;
+        else
+        {
+            if (c == '\n')
+                break;
+            other++;
+        }
     }
-  }
   
-  puts("\nYou typed:");
-  printf("A string with %d characters\n", length);
-  printf("\t%d lower case letters\n", lowercase);
-  printf("\t%d upper case letters\n", uppercase);
-  printf("\t%d digits\n", digits);
-  printf("\t%d others\n", other);
+    puts("\nYou typed:");
+    printf("A string with %d characters\n", length);
+    printf("\t%d lower case letters\n", lowercase);
+    printf("\t%d upper case letters\n", uppercase);
+    printf("\t%d digits\n", digits);
+    printf("\t%d others\n", other);
 }
