7b1b2d686adcbd2b3addc4eeed7ec9fd74f82329
Spencer Poisseroux <spoisseroux@me.com>
2020-02-17 16:02:16 -0500

    lab 3 : task 1

--- lab3/task1/uppercase.c
+++ lab3/task1/uppercase.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-/* example:
- *  char str[10090];
-   int ch, n = 0;
+int main() {
+    char str[10090];
+    int i;
+    
+    //input str to uppercase
+    printf("Input String: ");
+    fgets(str, 10090, stdin);
 
-   while ((ch = getchar()) != EOF && n < 1000) {
-      str[n] = ch;
-      ++n;
-   }
-
-   for (int i = 0; i < n; ++i)
-      putchar(str[i]);
-
-
-   return 0;
-
- */
-
+    //go thru each char of string
+    for (i=0; str[i] != '\0'; i++) {
+        //if it is an ASCII lowercase
+        if(str[i] >='a' && str[i] <= 'z') {
+            //to uppercase!
+            str[i] = str[i] - 32;
+        }
+    }
+    
+    //output new string
+    printf("Uppercase String: %s", str);
     return 0;
 }
 
