6515dd1f550faeed70ce21d55157b62f4b5a8723
Spencer Poisseroux <spoisseroux@me.com>
2020-03-03 15:42:16 -0500

    lab 5 : task 1

--- lab5/task1/construct_3_structs.c
+++ lab5/task1/construct_3_structs.c
@@ -4,22 +4,47 @@
 typedef struct snode node_t;
 
 node_t * setup() {
-    node_t * head ;
     //TODO:head declared for you
+    //node_t * head;
+    
     //Allocate three more pointers
     //head for the first Node, and temporary pointers node1, node2 and node3
-    //Allocate three node pointees and store references to them in the three pointers
-    //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
-    //Dereference each pointer to access the .next field in its pointee, 
-    //and use pointer assignment to set the .next field to point to the appropriate Node. 
+    node_t * node1;
+    node_t * node2;
+    node_t * node3;
     
-   return head;
+    
+    //Allocate three node pointees and store references to them in the three pointers
+    
+    node1 = calloc(6, sizeof(char));
+    node2 = calloc(6, sizeof(char));
+    node3 = calloc(5, sizeof(char));
+    
+    //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
+    node1->length = 5;
+    strcpy(node1->str, "hello");
+    
+    node2->length = 5;
+    strcpy(node2->str, "there");
+    
+    node3->length = 4;
+    strcpy(node3->str, "prof");
+    
+    //Dereference each pointer to access the .next field in its pointee,
+    //and use pointer assignment to set the .next field to point to the appropriate Node.
+   
+    //head->next = node1;
+    node1->next = node2;
+    node2->next = node3;
+    
+   return node1;
 }
 
-void teardown(/*what parameter?*/) {
+void teardown(node_t * head) {
     //TODO: free all dynamic memory you requested.
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
+    free(head);
 }
 
 //You can ignore the following code for testing
@@ -27,7 +52,7 @@ void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
     dump_all(head);
-    teardown(/*what argument?*/);
+    teardown(head);
     return 0;
 }
 
@@ -37,6 +62,7 @@ void dump_all(node_t * head) {
     while(cur != NULL) {
         printf("%s ", cur->str);
 	cur = cur->next;
+    free(cur);
     }
     printf("\ndone\n ");
 }
