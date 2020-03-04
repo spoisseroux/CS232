8a234dc6ae79cd57da7590278f6acfaff9eee5cd
Spencer Poisseroux <spoisseroux@me.com>
2020-03-03 19:35:58 -0500

    lab 5 : task 2 *mem leak 3 bytes*

--- lab5/task2/construct_3_structs_add.c
+++ lab5/task2/construct_3_structs_add.c
@@ -12,36 +12,31 @@ node_t * setup() {
     node_t * node1;
     node_t * node2;
     node_t * node3;
-    node_t * node4;
     
     
     //Allocate three node pointees and store references to them in the three pointers
     
-    node1 = calloc(3, sizeof(char));
-    node2 = calloc(6, sizeof(char));
-    node3 = calloc(6, sizeof(char));
-    node4 = calloc(5, sizeof(char));
+    node1 = calloc(1, sizeof(char));
+    node2 = calloc(1, sizeof(char));
+    node3 = calloc(1, sizeof(char));
     
     //Dereference each pointer to store the appropriate number and string into the length and str fields in its pointee.
-    node1->length = 2;
-    strcpy(node1->str, "hi");
+    
+    node1->length = 5;
+    strcpy(node1->str, "hello");
     
     node2->length = 5;
-    strcpy(node2->str, "hello");
+    strcpy(node2->str, "there");
     
-    node3->length = 5;
-    strcpy(node3->str, "there");
-    
-    node4->length = 4;
-    strcpy(node4->str, "prof");
+    node3->length = 4;
+    strcpy(node3->str, "prof");
+
     
     //Dereference each pointer to access the .next field in its pointee,
     //and use pointer assignment to set the .next field to point to the appropriate Node.
    
-    //head->next = node1;
     node1->next = node2;
     node2->next = node3;
-    node3->next = node4;
     
    return node1;
 }
@@ -51,17 +46,33 @@ void teardown(node_t * head) {
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
     free(head);
+    
 }
 
-void add(node_t ** head, char * str, int length){
+void add(node_t * head, char * str, int length){
     //TODO: implement add to add a new node to front, pointed by head
+    
+    //set head to new values
+    //copy head to new pointer
+    //point head to new
+    
+    node_t * new;
+    new = calloc(1, sizeof(char));
+    new->next = head->next;
+    strcpy(new->str, head->str);
+    new->length = head->length;
+    head->next = new;
+    head->length = length;
+    free(head);
+    head = calloc(1, sizeof(char));
+    strcpy(head->str, str);
 }
 
 //You can ignore the following code for testing
 void dump_all(node_t*);
 int main (int argc, char ** argv) {
     node_t * head = setup();
-    add(&head, "hi", 2);
+    add(head, "hi", 2);
     dump_all(head);
     teardown(head);
     return 0;
@@ -72,8 +83,9 @@ void dump_all(node_t * head) {
     node_t * cur = head;
     while(cur != NULL) {
         printf("%s ", cur->str);
-	cur = cur->next;
-        free(cur);
+        cur = cur->next;
+        teardown(cur);
     }
+    teardown(cur);
     printf("\ndone\n ");
 }
