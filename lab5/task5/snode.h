9164dcc8d8fd34a90ccfc239231084cf9170184e
Spencer Poisseroux <spoisseroux@me.com>
2020-03-07 20:38:44 -0500

    lab 5 : task 5 *mem leak*

---
+++ lab5/task5/snode.h
@@ -0,0 +1,20 @@
+#ifndef _snode_H_
+#define _snode_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct snode {
+  //TODO: change str to dynamic allcoation
+    //int length;
+    struct snode *next;
+    void* data;
+};
+
+//TODO: change prototypes of functions to remove
+//length
+struct snode* snode_create(void *s);
+void snode_destroy(struct snode * s);
+ 
+#endif
