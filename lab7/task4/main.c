b247869de23da5a2459ddfa99513344cb9e400be
Spencer Poisseroux <spoisseroux@me.com>
2020-04-06 19:40:33 -0400

    lab 7: task 4

--- lab7/task4/main.c
+++ lab7/task4/main.c
@@ -2,14 +2,9 @@ int main() {
     int a=13,b=5, c=0;
     if(a >6) {
         b = 15;
-    } else {
-        
     }
-    if (c < 14) {
+    while (c < 15) {
         a = a + c;
         c++;
-        
     }
 }
-
-//some jmp statements are off, couldnt figure out .Ls!
