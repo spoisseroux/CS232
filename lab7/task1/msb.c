997eee0363f9332a1b3bc20d3d6cc63e021cf26b
Spencer Poisseroux <spoisseroux@me.com>
2020-04-05 23:02:12 -0400

    lab 7: task 1

--- lab7/task1/msb.c
+++ lab7/task1/msb.c
@@ -12,8 +12,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 	int x=atoi(argv[1]);
-	printf("X= "); 
-	print_binary(x); 
+	printf("X= ");
+	print_binary(x);
 	printf("\n");
 	printf("The most significant bit of %d is at position %d\n",x,msb(x));
 	return 0;
@@ -29,152 +29,189 @@ int msb(int x) {
 	//                        ^
 	//                        |
 	//                        ep
-	// in which * is a wild card which could be 0 or 1                   
+	// in which * is a wild card which could be 0 or 1
 	int w, ep;
 	w=32; // Number of bits that might contain most significant 1
 	ep=0; // Rightmost bit that might contain most significant 1
-	while(w>1) { //Narrow down to a single bit
+	//while(w>1) { //Narrow down to a single bit
 		//TODO: Look at half the range of bits
         int hw = w/2;
 		//TODO: create a mask.
-        int mask = x & 4294901760;
+        int mask = 4294901760;
+				ep = 16;
         //11111111111111110000000000000000;
 		// This mask is all one-bits in the left half of the range
 		printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
 		//TODO: use mask to figure out if the left half of the range has at least one bit
-        
+
         //bits in left
-        if (mask != x) {
+        if ((mask&x) != 0) {
             //check left 2
-            mask = x & 4278190080;
+            mask = 4278190080;
+						hw = hw/2;
+						ep = 24;
+						printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
             //11111111000000000000000000000000;
             //bits in left 2
-            if (mask != x) {
+            if ((mask&x) != 0) {
                 //check left most
-                mask = x & 4026531840;
+                mask = 4026531840;
+								hw = hw/2;
+								ep = 28;
+								printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
                 //1111 0000 0000 0000 0000 0000 0000 0000;
                 //bits in left most
-                if (mask != x) {
-                    
+                if ((mask&x) != 0) {
+
                     for(int n=31;n>=27;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
                 }
 
-                
+
                 //bits in right
-                if (mask == x) {
-                    
+                if ((mask&x) == 0) {
+
                     for(int n=26;n>=23;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
                 }
-                
+
             //bits in right 2
-            if (mask == x) {
+            if ((mask&x) == 0) {
                 //check right 2
-                mask = x & 15728640;
+                mask = 15728640;
+								hw = hw/2;
+								ep = 20;
+								printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
                 //00000000111100000000000000000000;
-                
+
                 //check left
-                if (mask != x) {
-                    
+                if (((mask&x) != 0)) {
+
                     for(int n=23;n>=19;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
-                        
+
                 }
                 //check right
-                if (mask == x) {
-                    
+                if ((mask&x) == 0) {
+
                     for(int n=19;n>=15;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
-                    
+
                 }
             }
         }
         }
-        
+
         //bits in right
-        if (mask == x) {
+        if ((mask&x) == 0) {
+
             //check left 2
-            mask = x & 65280;
+            mask = 65280;
+						hw = hw/2;
+						ep = 8;
+						printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
             //00000000000000001111111100000000;
             //bits in left 2
-            if (mask != x) {
+            if (((mask&x) != 0)) {
                 //check left most
-                mask = x & 61440;
+                mask = 61440;
+								hw = hw/2;
+								ep = 12;
+								printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
                 //00000000000000001111000000000000;
-    
+
                 //bits in left most
-                if (mask != x) {
-                    
+                if (((mask&x) != 0)) {
+
                     for(int n=15;n>=11;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
-                        
+
                 }
-                
+
                 //bits in right
-                if (mask == x) {
-                   
+                if ((mask&x) == 0) {
+
                     for(int n=11;n>=7;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
-                        
+
                 }
-                
+
             }
-            
+
             //bits in right 2
-            if (mask == x) {
+            if ((mask&x) == 0) {
                 //check right 2
-                mask = x & 240;
+                mask = 240;
+								hw = hw/2;
+								ep = 4;
+								printf("M= "); print_binary(mask); printf(" hw=%d ep=%d\n",hw,ep);
                 //00000000000000000000000011110000;
-                
+
                 //check left
-                if (mask != x) {
-                    
+                if (((mask&x) != 0)) {
+
                     for(int n=7;n>=3;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
-                        
+
                 }
-                
+
                 //check right
-                if (mask == x) {
-                    
+                if ((mask&x) == 0) {
+
                     for(int n=3;n>=0;n--) {
                         if ((x & (1<<n))!= 0){
                             ep = n;
+														return ep;
+														w = 0;
                         }
                     }
                 }
             }
         }
-            
-        
-        
+
+
+
+
 		//If the left half of the range has a one bit, focus on only the left half
 		//else focus on only the right half
 		//TODO: You have ruled out either the left half of the range or the right half of the range
 		//set up ep and w accordingly
-	}
+	//}
 	return ep;
 }
 //Do not modify print_binary
