0f6de7fb39e99553417cef9b47e719771d4b5938
Spencer Poisseroux <spoisseroux@me.com>
2020-04-06 19:53:45 -0400

    lab 7: task 5

--- lab7/task5/main.c
+++ lab7/task5/main.c
@@ -1,13 +1,26 @@
 char password[8] = "secret";
 int main() {
     char input[8];
+    int i;
+
     printf("please enter your password\n");
     scanf("%s", input);
 
-    if(strcmp(input, password)==0) {
-        return 0;
-    } else {
-        return -1;
-    }
+    i = 0;
 
+    while (i < 8) {
+        i = i + i;
+
+        if ((int)&input >= 65) {
+            if ((int)&input < 91) {
+            
+            }
+        }
+        
+        if(strcmp(input, password)==0) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
 }
