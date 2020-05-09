da4a805f3a190f975c531fec9bcbce7aeccbb266
Spencer Poisseroux <spoisseroux@me.com>
2020-05-08 21:39:38 -0400

    lab 10 : task 1 -toPC

---
+++ lab10/task1/matrix_mul_jki.c
@@ -0,0 +1,33 @@
+#include "syscall.h"
+#include "stdio.h"
+#define Dim 	2048
+
+int A[Dim][Dim];
+int B[Dim][Dim];
+int C[Dim][Dim];
+
+int
+main()
+{
+    int i, j, k, r;
+
+    for (i = 0; i < Dim; i++)		/* first initialize the matrices */
+	for (j = 0; j < Dim; j++) {
+	     A[i][j] = i;
+	     B[i][j] = j;
+	     C[i][j] = 0;
+	}
+
+    for (j = 0; j < Dim; j++){
+        for (k = 0; k < Dim; k++) {
+            r = B[k][j];
+            for (i = 0; i < Dim; i++){
+                C[i][j] += A[i][k] * r;
+            }
+        }
+    }
+
+    printf("C[%d][%d] = %d\n", Dim-1, Dim-1, C[Dim-1][Dim-1]);
+    return (C[Dim-1][Dim-1]);		/* and then we're done */
+}
+
